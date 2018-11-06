0e55fe045c73c2989312a06072f9e3390639b786
Konrad Kolpak <kolpak.conrad@gmail.com>
2018-11-06 23:00:50 +0000

    web sockets and simple join screen added

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express      = require( "express"   );
+var socket_io    = require( "socket.io" );
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// hello world
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+// Socket.io
+var io           = socket_io();
+app.io           = io;
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +18,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/public/html/intro.html'));
+});
+
+
+app.get('/speechTest', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/public/html/index.html'));
+});
+
+// socket.io events
+io.on( "connection", function( socket )
+{
+    console.log( "A user connected" );
+});
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('/intro.html');
+});
+
+
+/* GET home page. */
+router.get('/speechTest', function(req, res, next) {
+  res.render('index');
 });
 
 module.exports = router;
